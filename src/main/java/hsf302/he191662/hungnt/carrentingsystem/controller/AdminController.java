4dd641e89b11f02366d472d1ded9f2f394f57b6b
hungqt9x78 <nthung06082k5@gmail.com>
2025-10-24 08:53:30 +0700

    feat: add update customer infomation

--- src/main/java/hsf302/he191662/hungnt/carrentingsystem/controller/AdminController.java
+++ src/main/java/hsf302/he191662/hungnt/carrentingsystem/controller/AdminController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @Controller
@@ -36,7 +37,7 @@ public class AdminController {
     public String customers(HttpServletRequest request, HttpServletRequest response, HttpServletRequest session, Model model) {
         String customerIdFilter = request.getParameter("customerIdFilter");
         String customerNameFilter = request.getParameter("customerNameFilter");
-        List<Customer> customers = customerService.findByCustomerIdAndCustomerName(customerIdFilter,customerNameFilter);
+        List<Customer> customers = customerService.findByCustomerIdAndCustomerName(customerIdFilter, customerNameFilter);
         model.addAttribute("customerIdFilter", customerIdFilter);
         model.addAttribute("customerNameFilter", customerNameFilter);
         model.addAttribute("customers", customers);
@@ -50,7 +51,7 @@ public class AdminController {
 
     @PostMapping("/customers/create")
     public String createCustomerP(Model model, RedirectAttributes redirectAttributes, HttpServletRequest request) {
-        Validation  valid= new Validation();
+        Validation valid = new Validation();
         String username = request.getParameter("username").trim();
         String password = request.getParameter("password").trim();
         String email = request.getParameter("email").trim();
@@ -61,43 +62,149 @@ public class AdminController {
         model.addAttribute("customerName", customerName);
 
 
-        if(username==null||username.trim().isEmpty()){
+        if (username == null || username.trim().isEmpty()) {
             model.addAttribute("error", "Tên đăng nhập không đự bỏ trống.");
             return "admin/customer-create";
-        }else if(accountService.existsByUsername(username)){
-            model.addAttribute("error","Tên đăng nhập đã tồn tại.");
+        } else if (accountService.existsByUsername(username)) {
+            model.addAttribute("error", "Tên đăng nhập đã tồn tại.");
             return "admin/customer-create";
         }
 
-        if(password==null||password.trim().isEmpty()||password.length()<6){
-            model.addAttribute("error","Mật khẩu không hợp lệ.");
+        if (password == null || password.trim().isEmpty() || password.length() < 6) {
+            model.addAttribute("error", "Mật khẩu không hợp lệ.");
             return "admin/customer-create";
         }
 
-        if(email==null||email.trim().isEmpty()||!valid.isValidEmail(email)){
+        if (email == null || email.trim().isEmpty() || !valid.isValidEmail(email)) {
             model.addAttribute("error", "Email không hợp lệ.");
             return "admin/customer-create";
-        }else if(accountService.existsByEmail(email)){
-            model.addAttribute("error","Email đã tồn tại.");
+        } else if (accountService.existsByEmail(email)) {
+            model.addAttribute("error", "Email đã tồn tại.");
             return "admin/customer-create";
         }
 
-        if(customerName==null||customerName.trim().isEmpty()){
-            model.addAttribute("error","Tên Khách hàng không được bỏ trống");
+        if (customerName == null || customerName.trim().isEmpty()) {
+            model.addAttribute("error", "Tên Khách hàng không được bỏ trống");
             return "admin/customer-create";
         }
         Account account = new Account(username.trim(), password.trim(), email.trim(), "customer", true);
-        Customer customer = new Customer(customerName.trim(),email.trim(), password.trim(),0.0);
+        Customer customer = new Customer(customerName.trim(), email.trim(), password.trim(), 0.0);
         account.setCustomer(customer);
         customer.setAccount(account);
         accountService.save(account);
         model.addAttribute("success", "Tạo khách hàng mới thành công");
         return "admin/customer-create";
     }
+
     @GetMapping("/customers/update")
-    public String updateCustomer() {
+    public String updateCustomer(HttpServletRequest request, Model model, RedirectAttributes redirectAttributes) {
+
+        String customerId = request.getParameter("customerId");
+        try {
+            Long id = Long.parseLong(customerId);
+            Customer customer = customerService.findByCustomerId(id);
+            model.addAttribute("customer", customer);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Không tồn tại khách hàng này");
+            return "redirect:/admin/customers";
+        }
         return "admin/customer-update";
     }
 
+    @PostMapping("customers/update")
+    public String updateCustomerP(HttpServletRequest request, Model model, RedirectAttributes redirectAttributes) {
+        Validation valid = new Validation();
+        try {
+            Long id = Long.parseLong(request.getParameter("customerId"));
+            Customer customer = customerService.findByCustomerId(id);
+            model.addAttribute("customer", customer);
+            if (customer == null) {
+                redirectAttributes.addFlashAttribute("error", "Không tồn tại khách hàng này");
+                return "redirect:/admin/customers";
+            }
+
+            String customerName = request.getParameter("customerName").trim();
+            String email = request.getParameter("email").trim();
+            String password = request.getParameter("password").trim();
+            String mobile = (request.getParameter("mobile") != null) ? request.getParameter("mobile").trim() : "";
+            String identityCard = (request.getParameter("identityCard") != null) ? request.getParameter("identityCard").trim() : "";
+            String licenceNumber = (request.getParameter("licenceNumber") != null) ? request.getParameter("licenceNumber").trim() : "";
+            String licenceDate = request.getParameter("licenceDate");
+
+            if (customerName.isEmpty()) {
+                model.addAttribute("error", "Tên khách hàng không hợp lệ.");
+                return "admin/customer-update";
+            }
+
+            if (email.isEmpty() || !valid.isValidEmail(email)) {
+                model.addAttribute("error", "Email không hợp lệ.");
+                return "admin/customer-update";
+            } else if (accountService.existsByEmail(email) && !email.equals(customer.getEmail())) {
+                model.addAttribute("error", "Email đã tồn tại.");
+                return "admin/customer-update";
+            }
+
+            if (password.isEmpty() || password.length() < 6) {
+                model.addAttribute("error", "Mật khẩu không hợp lệ.");
+                return "admin/customer-update";
+            }
+
+            if (!mobile.isEmpty() && !valid.isValidVietnamPhone(mobile)) {
+                model.addAttribute("error", "Số điện thoại không hợp lệ.");
+                return "admin/customer-update";
+            }
+
+            if (!identityCard.isEmpty()) {
+                if (identityCard.length() != 12) {
+                    model.addAttribute("error", "CCCD phải có 12 ký tự");
+                    return "admin/customer-update";
+                }
+
+                if(customerService.findByIdentityCard(identityCard)!=null){
+                    model.addAttribute("error", "CCCD đã tồn tại");
+                    return "admin/customer-update";
+                }
+
+            }
+
+            if (!licenceNumber.isEmpty()) {
+                if (licenceNumber.length() != 12) {
+                    model.addAttribute("error", "GPLX phải có 12 ký tự");
+                    return "admin/customer-update";
+                }
+
+                if(customerService.findByLicenceNumber(licenceNumber)!=null){
+                    model.addAttribute("error", "GPLX đã tồn tại");
+                    return "admin/customer-update";
+                }
+
+            }
+
+            try {
+                if (licenceDate != null && !licenceDate.trim().isEmpty()) {
+                    customer.setLicenceDate(LocalDate.parse(licenceDate));
+                }
+            } catch (DateTimeParseException e) {
+                model.addAttribute("error", "Ngày cấp GPLX không hợp lệ.");
+                return "admin/customer-update";
+            }
+
+            // Cập nhật thông tin
+            customer.setCustomerName(customerName);
+            customer.setEmail(email);
+            customer.setPassword(password);
+            customer.setMobile(mobile);
+            customer.setIdentityCard(identityCard == "" ? null : identityCard);
+            customer.setLicenceNumber(licenceNumber == "" ? null : licenceNumber);
+
+            customerService.save(customer);
+            redirectAttributes.addFlashAttribute("success", "Cập nhật thông tin thành công");
+            return "redirect:/admin/customers";
+
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Không tồn tại khách hàng này");
+            return "redirect:/admin/customers";
+        }
+    }
 
 }
